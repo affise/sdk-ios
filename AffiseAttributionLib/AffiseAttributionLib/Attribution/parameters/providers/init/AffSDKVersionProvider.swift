fa8fadc0b8243cb867451450b5ac7b0985314fed
Sergey Korney <s.korney@affise.com>
2023-07-25 01:39:23 +0500

    1.6.2

--- AffiseAttributionLib/AffiseAttributionLib/Attribution/parameters/providers/init/AffSDKVersionProvider.swift
+++ AffiseAttributionLib/AffiseAttributionLib/Attribution/parameters/providers/init/AffSDKVersionProvider.swift
@@ -14,7 +14,7 @@ import Foundation
 class AffSDKVersionProvider: StringPropertyProvider {
     
     override func provide() -> String? {
-        return "1.6.1"
+        return "1.6.2"
     }
     
     override func getOrder() -> Float {
